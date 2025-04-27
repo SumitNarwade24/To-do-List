3afb3a6dac9eda09dddef7f2c162098e75ac9347
SumitNarwade24 <narwadesumit2021@gmail.com>
2025-04-28 00:02:18 +0530

    Resolved all Merge Conflicts

--- app.js
+++ app.js
@@ -93,8 +93,8 @@ let updateList = ()=>{
                 <p>${task.text}</p>
             </div>
             <div class="icons">
-                <img src="LICENSE/edit.png" onClick="editTask(${index});"/>
-                <img src="LICENSE/delete.png" onClick="DeleteTask(${index});"/>
+                <img src="Images/edit.png" onClick="editTask(${index});"/>
+                <img src="Images/delete.png" onClick="DeleteTask(${index});"/>
             
             </div>
         </div>
